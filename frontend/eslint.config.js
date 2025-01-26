46d599e3b72470c4f95de41bb3ae0ed8576f73ad
Prateek61 <prateekpoudel61@gmail.com>
2025-01-26 19:13:16 +0545

    switched to skeletonUI

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,10 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...svelte.configs['flat/recommended'],
 	prettier,
@@ -20,5 +16,8 @@ export default [
 				...globals.node
 			}
 		}
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/']
 	}
 ];
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script>
-	let { children } = $props();
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow }); 
 </script>
 
-<div class="app">
-	<main>
-		{@render children()}
-	</main>
-</div>
-
-<style>
-	
-</style>
+<slot />
