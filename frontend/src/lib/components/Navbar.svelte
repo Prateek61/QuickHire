35cc9f8af23c2263d4c11c2334fdeef4375ff621
PrajwalChaudhary25 <prajwalchy25@gmail.com>
2025-02-21 14:56:39 +0545

    Adding home, login, about and signup page

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,56 @@
+<script>
+	let searchQuery = "";
+	import SearchIcon from '$lib/images/search.png';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	let activePath = '';
+
+	onMount(() => {
+		page.subscribe(($page) => {
+			activePath = $page.url.pathname;
+		});
+	});
+</script>
+
+<nav class="flex justify-between items-center min-h-[10vh] bg-[#23486a] font-[Poppins,sans-serif]">
+	<!-- Logo -->
+	<span class="text-[#efb036] text-[1.6rem] mx-[20px] font-bold font-[cursive]">QuickHire</span>
+
+	<!-- Navigation Links -->
+	<ul class="flex list-none items-center">
+		<li>
+			<a href="/" class="text-white no-underline text-[1.1rem] mx-[10px] py-[6px] px-[15px] rounded-[5px] transition-all duration-300 ease-in-out hover:bg-[#efb036] hover:text-[#23486a] {activePath === '/' ? 'bg-[#efb036] text-[#23486a]' : ''}">Home</a>
+		</li>
+
+		{#if activePath === '/' || activePath === '/about'|| activePath === '/login'}
+			<li>
+				<a href="/about" class="text-white no-underline text-[1.1rem] mx-[10px] py-[6px] px-[15px] rounded-[5px] transition-all duration-300 ease-in-out hover:bg-[#efb036] hover:text-[#23486a] {activePath === '/about' ? 'bg-[#efb036] text-[#23486a]' : ''}">About</a>
+			</li>
+			<li>
+				<a href="/login" class="text-white no-underline text-[1.1rem] mx-[10px] py-[6px] px-[15px] rounded-[5px] transition-all duration-300 ease-in-out hover:bg-[#efb036] hover:text-[#23486a] {activePath === '/login' ? 'bg-[#efb036] text-[#23486a]' : ''}">Login</a>
+			</li>
+		{:else}
+			<li>
+				<a href="/profiles" class="text-white no-underline text-[1.1rem] mx-[10px] py-[6px] px-[15px] rounded-[5px] transition-all duration-300 ease-in-out hover:bg-[#efb036] hover:text-[#23486a] {activePath === '/profiles' ? 'bg-[#efb036] text-[#23486a]' : ''}">Profiles</a>
+			</li>
+
+			<li>
+				<a href="/hire" class="text-white no-underline text-[1.1rem] mx-[10px] py-[6px] px-[15px] rounded-[5px] transition-all duration-300 ease-in-out hover:bg-[#efb036] hover:text-[#23486a] {activePath === '/hire' ? 'bg-[#efb036] text-[#23486a]' : ''}">Hire</a>
+			</li>
+					<!-- Search Bar -->
+			<div class="flex center mr-[20px]">
+				<input
+					type="text"
+					bind:value={searchQuery}
+					placeholder="Search.."
+					class="h-[35px] w-[200px] border-none rounded-[20px] pl-[10px]"
+				/>
+				<button class="h-[35px] w-[50px] border-none bg-[#efb036] text-[#23486a] font-bold cursor-pointer rounded-tr-[20px] rounded-br-[20px] ml-[-50px]">
+					<img class="ml-[12px] h-[20px] w-[20px]" src="{SearchIcon}" alt="Search Icon" />
+				</button>
+			</div>
+		{/if}
+
+	</ul>
+</nav>
---
+++ frontend/src/routes/(App)/login/+page.svelte
@@ -0,0 +1,15 @@
+  <script>
+    import LoginForm from './Login.svelte';
+    import penguin from '$lib/images/penguin.png';
+  </script>
+  
+  <section class="bg-slate-50 flex justify-between items-center min-h-[90vh] font-cambria " >
+    
+    <div class="flex-1 flex-col justify-center items-center max-w-[80%] h-[90vh]">
+        <img  class = "mx-[auto] mt-[135px] mb-[20px] w-[120px]" src="{penguin}" alt="QuickHire Logo">
+        <h1 class="text-center text-[3.5rem] text-[#f5a60a] mb-[20px]" >Welcome to QuickHire</h1>
+        <p class="text-[1.5rem] text-[#0840a7] italic mb-[20px] mt-[20px] ml-[250px]" >-A platform to find Professionals</p>
+    </div>
+    <LoginForm class="flex-1"/>
+
+  </section>
